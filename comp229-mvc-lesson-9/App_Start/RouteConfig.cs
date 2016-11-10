c9a1aed32b5f5e0e4c7c82169e2c409911679567
Thiago Andrade <thiagoh@gmail.com>
2016-11-09 21:59:02 -0500

    removing home controller

--- comp229-mvc-lesson-9/App_Start/RouteConfig.cs
+++ comp229-mvc-lesson-9/App_Start/RouteConfig.cs
@@ -13,7 +13,7 @@ namespace comp229_mvc_lesson_9 {
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Store", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- comp229-mvc-lesson-9/Controllers/GenresController.cs
+++ comp229-mvc-lesson-9/Controllers/GenresController.cs
@@ -8,120 +8,25 @@ using System.Web;
 using System.Web.Mvc;
 using comp229_mvc_lesson_9.Models;
 
-namespace comp229_mvc_lesson_9.Controllers
-{
-    public class GenresController : Controller
-    {
+namespace comp229_mvc_lesson_9.Controllers {
+    public class GenresController : Controller {
         private MVCMusicStoreContext db = new MVCMusicStoreContext();
 
         // GET: Genres
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View(db.Genres.ToList());
         }
 
         // GET: Genres/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Genre genre = db.Genres.Find(id);
-            if (genre == null)
-            {
+            if (genre == null) {
                 return HttpNotFound();
             }
             return View(genre);
-        }
-
-        // GET: Genres/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Genres/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GenreId,Name,Description")] Genre genre)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Genres.Add(genre);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(genre);
-        }
-
-        // GET: Genres/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Genre genre = db.Genres.Find(id);
-            if (genre == null)
-            {
-                return HttpNotFound();
-            }
-            return View(genre);
-        }
-
-        // POST: Genres/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "GenreId,Name,Description")] Genre genre)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(genre).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(genre);
-        }
-
-        // GET: Genres/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Genre genre = db.Genres.Find(id);
-            if (genre == null)
-            {
-                return HttpNotFound();
-            }
-            return View(genre);
-        }
-
-        // POST: Genres/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Genre genre = db.Genres.Find(id);
-            db.Genres.Remove(genre);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
