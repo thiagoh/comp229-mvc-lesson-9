ff374a7d123072405aff1f13756b78c4da07c896
Thiago Andrade <thiagoh@gmail.com>
2016-11-09 20:53:49 -0500

    Add project files.

---
+++ comp229-mvc-lesson-9/Startup.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(comp229_mvc_lesson_9.Startup))]
+namespace comp229_mvc_lesson_9 {
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
